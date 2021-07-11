0e0656993e12d08c972b44467b70ffc5bfe95225
Yenting Chen <yeinbo0620@gmail.com>
2021-07-12 04:34:16 +0800

    Create Two Sum.swift

---
+++ Two Sum.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        var dic: [Int : Int] = [:]
+        
+        for (i, num) in nums.enumerated() {
+            
+            let second = target - num
+            
+           
+            
+            if let secondIndex = dic[second] {
+                
+                return [ i, secondIndex]
+                
+            } else {
+                
+                dic[num] = i
+                
+            }
+            
+        }
+        
+        return []
+    }
+}
+
+let solution = Solution()
+
+print(solution.twoSum([3,3] ,6 ))
